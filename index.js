8a7eb2aa7e7148a312d54e40421ae05e0c21ef90
Aymen <111906131+Aymen66@users.noreply.github.com>
2022-11-11 18:22:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,31 +2,31 @@ let convertBtn = document.getElementById("convertBtn")
 
 
 meterBtn.addEventListener("click", function(){
-  let input = document.getElementById("input").value;
-  document.getElementById("meterOutput").value = Number (input/ 3.281).toFixed(3)
+  let meterInput = document.getElementById("meterInput").value;
+  document.getElementById("meterOutput").value = Number (meterInput/ 3.281).toFixed(3)
 })
 feetBtn.addEventListener("click", function(){
-  let input = document.getElementById("input").value;
-  document.getElementById("feetOutput").value = Number (input *3.281).toFixed(3)
+  let feetInput = document.getElementById("feetInput").value;
+  document.getElementById("feetOutput").value = Number (feetInput *3.281).toFixed(3)
 })
 
 litersBtn.addEventListener("click", function(){
-  let input = document.getElementById("input").value;
-  document.getElementById("litersOutput").value = Number(input * 4.546).toFixed(3)
+  let litersInput = document.getElementById("litersInput").value;
+  document.getElementById("litersOutput").value = Number(litersInput * 4.546).toFixed(3)
 })
 
 gallonsBtn.addEventListener("click", function(){
-  let input = document.getElementById("input").value;
-  document.getElementById("gallonsOutput").value = Number(input / 4.546).toFixed(3)
+  let gallonsInput = document.getElementById("gallonsInput").value;
+  document.getElementById("gallonsOutput").value = Number(gallonsInput / 4.546).toFixed(3)
 })
 
 
 kilogramsBtn.addEventListener("click", function(){
-  let input = document.getElementById("input").value;
-  document.getElementById("kilogramsOutput").value = Number(input / 2.2046).toFixed(3)
+  let kilogramsInput = document.getElementById("kilogramsInput").value;
+  document.getElementById("kilogramsOutput").value = Number(kilogramsInput / 2.2046).toFixed(3)
 })
 
 poundsBtn.addEventListener("click", function(){
-  let input = document.getElementById("input").value;
-  document.getElementById("poundsOutput").value = Number(input * 2.2046).toFixed(3)
+  let poundsInput = document.getElementById("poundsInput").value;
+  document.getElementById("poundsOutput").value =Number(poundsInput * 2.2046).toFixed(3)
 })
